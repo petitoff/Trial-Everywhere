f3deda11057cd39b202d3983491eca92a02efcb9
Błażej Domagała <petitoff@outlook.com>
2022-04-02 18:55:06 +0000

    Checking if the card number is correct.

--- Trial-Everywhere/CreditCardNumberGenerator.cs
+++ Trial-Everywhere/CreditCardNumberGenerator.cs
@@ -6,8 +6,46 @@ using System.Threading.Tasks;
 
 namespace Trial_Everywhere
 {
-    class CreditCardNumberGenerator
+    internal class CreditCardNumberGenerator
     {
+        public CreditCardNumberGenerator()
+        {
 
+        }
+
+        public string GetCreditCardNumbers(string prefix)
+        {
+            return null;
+        }
+
+        private bool IsValidCreditCardNumber(string creditCardNumber)
+        {
+            //string creditCard = "5297501100132586";
+            List<int> creditCardList = creditCardNumber.Select(i => Convert.ToInt32(i.ToString())).ToList();
+
+            int sumAllNumbers = 0;
+            for (int i = 0; i < creditCardList.Count(); i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sumAllNumbers += creditCardList[i] * 2;
+                }
+                else
+                {
+                    sumAllNumbers += creditCardNumber[i];
+                }
+            }
+
+            return sumAllNumbers % 10 == 0;
+        }
+
+        private string ReverseString(string text)
+        {
+            if (text == null) return null;
+
+            char[] array = text.ToCharArray();
+            Array.Reverse(array);
+            return new String(array);
+        }
     }
 }
